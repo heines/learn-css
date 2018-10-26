26cd895fe9ec0d5989cf4ff20485373deec2d828
heine <heine.shimizu@gmail.com>
2018-10-26 11:05:53 +0900

    作業環境設定

---
+++ learn-css-dev/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloCSS />
+  </div>
+</template>
+
+<script>
+import HelloCSS from './components/HelloCSS'
+
+export default {
+  name: 'App',
+  components: {
+    HelloCSS
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ learn-css-dev/src/components/HelloCss.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div.hello
+    h1 Learn CSS
+</template>
+
+<script>
+export default {
+  name: 'HelloCSS',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
