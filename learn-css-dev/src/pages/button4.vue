3daa6a93cc2e186da0ce9ac9b87db723f3674ed0
heine <heine.shimizu@gmail.com>
2018-11-06 01:53:06 +0900

    add button.

--- learn-css-dev/src/pages/button4.vue
+++ learn-css-dev/src/pages/button4.vue
@@ -2,19 +2,16 @@
   .base-box
     h2.title 復習
     .date-box
-      p.date-box_text 2018 Nov 4th
-    .desc-box
-      p.desc-box_text
-        |ボタンを作る。
-        br
-        |FLOCSSに合わせてリファクタリングしようとしてるんだけど、なかなか難しい。
-        br
-        |とりあえずObjectフォルダを作ってみた。
+      p.date-box_text 2018 Nov 4th / 2018 Nov 5th
     .contents__large
       .contents-section__row
         a(href="").c-btn.p-btn__mdl 上に戻る
-        a(href="").c-btn.p-btn_sml 上
-        a(href="").c-btn.p-btn_top 上
+        a(href="").c-btn.p-btn__sml 上
+        a(href="").c-btn.p-btn__top 上
+      .contents-section__row
+        a(href="").c-btn.p-btn__cross 上に戻る
+      .contents-section__row
+        a(href="").c-btn.p-btn__bar 上に戻る
 </template>
 
 <script>
