82aecc06c93b2b8f8bf235fd9b355c72ff66afa0
heine <heine.shimizu@gmail.com>
2018-11-28 19:15:20 +0900

    画像のパスをgithub.ioに対応（修正）

--- learn-css-dev/config/index.js
+++ learn-css-dev/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../..'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '../../',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
