97a4143c6155cd2aa1b07b563800ff1ccfd8fd7a
heine <heine.shimizu@gmail.com>
2019-01-26 21:11:24 +0900

    add modifier.

--- learn-css-dev/src/pages/drawPage.vue
+++ learn-css-dev/src/pages/drawPage.vue
@@ -4,10 +4,15 @@
       .p-slide-show__image1
       .p-slide-show__image2
       .p-slide-show__image3
-    .c-square-box
-      .c-square-box__content-wrap
-        .c-square-box__content
-          img(src='../assets/pakutaso001.jpg')
+    .c-flexbox
+      .c-square-box.c-square-box--bg
+        .c-square-box__content-wrap
+          .c-square-box__content
+            img(src='../assets/pakutaso001.jpg')
+      .c-square-box.c-square-box--frame
+        .c-square-box__content-wrap
+          .c-square-box__content
+            p 必ず正方形になる
 </template>
 
 <script>
@@ -30,6 +35,7 @@
 <style lang="scss" scoped>
   @import '../css/Object/Project/_slide-show.scss';
   @import '../css/Object/Component/_square_box.scss';
+  @import '../css/Object/Component/_flexbox.scss';
   h1, h2 {
     font-weight: normal;
   }
