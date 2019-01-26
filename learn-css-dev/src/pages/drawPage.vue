84a1590cb6b9cbf7e91bb732306726bfc9a472d7
heine <heine.shimizu@gmail.com>
2019-01-26 21:01:25 +0900

    add square-box.

--- learn-css-dev/src/pages/drawPage.vue
+++ learn-css-dev/src/pages/drawPage.vue
@@ -4,6 +4,10 @@
       .p-slide-show__image1
       .p-slide-show__image2
       .p-slide-show__image3
+    .c-square-box
+      .c-square-box__content-wrap
+        .c-square-box__content
+          img(src='../assets/pakutaso001.jpg')
 </template>
 
 <script>
@@ -25,6 +29,7 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
   @import '../css/Object/Project/_slide-show.scss';
+  @import '../css/Object/Component/_square_box.scss';
   h1, h2 {
     font-weight: normal;
   }
